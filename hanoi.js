05318256a07dad6e5db8003c7a4a8aac57a93761
Livvie Lin <livvie.lin@gmail.com>
2015-08-29 16:45:53 -0700

    Implements hanoi

--- hanoi.js
+++ hanoi.js
@@ -2,52 +2,12 @@ var a = [];
 var b = [];
 var c = [];
 
-// step 1: solve for one disk, two disks, three disks
-var hanoi = function ( numDisks, fromPeg, toPeg, squarePeg ) {
-  var numMoves = 0;
-  // create the game
-  start( numDisks, fromPeg );
-
-  var miniHanoi = function ( numDisks, fromPeg, toPeg, squarePeg ) {
-    // base case when no disks left to move
-    if ( toPeg.length === numDisks ) {
-      console.log( fromPeg, toPeg, squarePeg );
-      console.log( numMoves );
-      return;
-    }
-    // solve for one disk
-    if ( numDisks === 1 ) {
-      moveDisk( fromPeg, toPeg );
-      numMoves += 1;
-      console.log( fromPeg, toPeg, squarePeg );
-      console.log( numMoves );
-      return;
-    }
-
-    // solve for two disks
-    if ( numDisks === 2 ) {
-      moveDisk( fromPeg, squarePeg );
-      numMoves += 1;
-      moveDisk( fromPeg, toPeg );
-      numMoves += 1;
-      moveDisk( squarePeg, toPeg );
-      numMoves += 1;
-      console.log( fromPeg, toPeg, squarePeg );
-      console.log( numMoves );
-      return;
-    }
-  };
-  
-  // solve for three disks
-  // solve for 2, but switch to and square pegs
-  miniHanoi( numDisks - 1, fromPeg, squarePeg, toPeg );
-  // solve for one disk to move last disk
-  miniHanoi( 1, fromPeg, toPeg, squarePeg );
-  // solve for 2 disks, but switch the from and square pegs
-  miniHanoi( numDisks - 1, squarePeg, toPeg, fromPeg );
-
-  // recurse
-  // hanoi( numDisks, fromPeg, toPeg, squarePeg, numMoves );
+var hanoi = function ( numDisks, fromPeg, toPeg, sparePeg ) {
+  if ( numDisks > 0 ) {
+    hanoi( numDisks - 1, fromPeg, sparePeg, toPeg );
+    moveDisk( fromPeg, toPeg );
+    hanoi( numDisks - 1, sparePeg, toPeg, fromPeg );
+  }
 };
 
 // function to populate fromPeg, with smallest disk at last index
@@ -70,6 +30,9 @@ var moveDisk = function ( fromPeg, toPeg ) {
 // test move disk function
 // moveDisk( a, b );
 // test hanoi recursion
-hanoi( 3, a, b, c );
+var numDisks = 5;
+start( numDisks, a );
+hanoi( numDisks, a, b, c );
+console.log( a, b, c );
 // view hanoi
 //console.log(a, b, c);
