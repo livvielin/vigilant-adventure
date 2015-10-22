1e2600041dfe63b75d95593bc500989443152022
Livvie Lin <livvie.lin@gmail.com>
2015-10-21 21:56:55 -0700

    Solve wordPattern

--- wordPattern.js
+++ wordPattern.js
@@ -5,13 +5,19 @@
  */
 var wordPattern = function(pattern, str) {
   var patternStorage = {};
+  var wordStorage = {};
   var strArray = str.split(' ');
   if (pattern.length !== strArray.length) {
     return false;
   }
   for (var i = 0; i < pattern.length; i++) {
     if (!patternStorage[pattern[i]]) {
-      patternStorage[pattern[i]] = strArray[i];
+      if (wordStorage[strArray[i]]) {
+        return false;
+      } else {
+        patternStorage[pattern[i]] = strArray[i];
+        wordStorage[strArray[i]] = true;
+      }
     } else {
       if (patternStorage[pattern[i]] !== strArray[i]) {
         return false;
