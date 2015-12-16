439e8e5daa3cb96c07db670e243bd5022ccec538
Livvie Lin <livvie.lin@gmail.com>
2015-12-15 16:04:25 -0800

    Set up reverse integer function

---
+++ reverseInteger.js
@@ -0,0 +1,12 @@
+/**
+ * @param {number} x
+ * @return {number}
+ */
+
+// function to reverse digits of an integer
+var reverse = function (x) {
+  // take mod to figure out how many places it has
+  // 123 -> 321, 300 + 20 + 1
+  // 100 -> 1, 0 + 0 + 1
+  // if number is negative
+};
