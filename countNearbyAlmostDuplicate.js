7c914158e842734a2877617bb9f4f7e176a02c08
Livvie Lin <livvie.lin@gmail.com>
2015-12-21 09:17:48 -0800

    Set up containsNearbyAlmostDuplicate

---
+++ countNearbyAlmostDuplicate.js
@@ -0,0 +1,12 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @param {number} t
+ * @return {boolean}
+ Given an array of integers, find out whether there are two distinct indices 
+ i and j in the array such that the difference between nums[i] and nums[j] 
+ is at most t and the difference between i and j is at most k.
+ */
+var containsNearbyAlmostDuplicate = function(nums, k, t) {
+  //
+};
