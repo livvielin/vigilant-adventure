bc6ea431fcc21453127fb746761f7e0388660cdb
Livvie Lin <livvie.lin@gmail.com>
2015-12-28 12:40:16 -0800

    Set up function to condense meeting times

---
+++ condenseMeetingTimes.js
@@ -0,0 +1,10 @@
+// function to condense meeting time ranges
+// input: array of tuples representing meeting times
+// integers in tuples represent 30-min blocks past 9am
+// output: condensed ranges
+// example: [(0, 1), (3, 5), (4, 8), (10, 12), (9, 10)]
+// -> [(0, 1), (3, 8), (9, 12)]
+// meetings not necessarily in order
+var condenseMeetingTimes = function (ranges) {
+  //
+};
