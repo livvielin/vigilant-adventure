d3a2920fb2341cbb6fbdbde02331b7b6591175f9
Livvie Lin <livvie.lin@gmail.com>
2016-01-01 00:45:21 -0800

    Set up superbalanced binary tree function

---
+++ superBalanced.js
@@ -0,0 +1,11 @@
+// function to determine whether difference between depths
+// of any two leaf nodes in a binary tree is no greater than 1
+var superBalanced = function (root) {
+  //
+};
+
+var Node = function (value) {
+  this.value = value;
+  this.right = null;
+  this.left = null;
+};
